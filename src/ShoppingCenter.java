a9784661b3c1607462edc61d4ecaa7e5012e9e4f
Tom Soistmann <soistmant5@students.rowan.edu>
2016-11-22 17:49:21 -0500

    Second commit

--- src/ShoppingCenter.java
+++ src/ShoppingCenter.java
@@ -14,6 +14,8 @@ public class ShoppingCenter {
     private QueueArrayBased expressCheckout;
 
     public ShoppingCenter() {
+        normalCheckout1 = new QueueArrayBased();
+        // FUCK github
 
     }
 }
